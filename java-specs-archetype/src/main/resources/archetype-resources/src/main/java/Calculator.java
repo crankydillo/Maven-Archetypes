81a6404823ddae8348391297b3cf25c41c674e12
Samuel Cox <crankydill@gmail.com>
2010-07-18 11:35:45 -0500

    java specs archetype

---
+++ java-specs-archetype/src/main/resources/archetype-resources/src/main/java/Calculator.java
@@ -0,0 +1,19 @@
+package ${groupId};
+
+import java.util.*;
+
+public class Calculator {
+  private LinkedList<String> history = new LinkedList<String>();
+  public Calculator() {}
+
+  public Integer add(Integer left, Integer right) {
+    history.addFirst(left + " + " + right);
+    if (left == null || right == null)
+      throw new IllegalArgumentException("Neither the left or the right operand may be null.");
+    return left + right;
+  }
+
+  public List<String> history() {
+    return Collections.unmodifiableList(history);
+  }
+}
